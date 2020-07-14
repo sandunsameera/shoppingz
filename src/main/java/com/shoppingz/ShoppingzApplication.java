bfabf9089f2ecf2cf4c50e8e9daaac8d59f5e939
ishara_it17099578 <it17099578@my.sliit.lk>
2020-07-14 23:01:01 +0530

    innitialize project

---
+++ src/main/java/com/shoppingz/ShoppingzApplication.java
@@ -0,0 +1,24 @@
+package com.shoppingz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class ShoppingzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingzApplication.class, args);
+
+    }
+
+    @RestController
+    public static class HomeController {
+        @RequestMapping("/")
+        public String index() {
+            return "index.html";
+        }
+    }
+
+}
---
+++ src/test/java/com/shoppingz/ShoppingzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
